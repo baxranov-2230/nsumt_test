fc77bd408c6e3261842722a9a691055a45d292b4
baxranov-2230 <axrorwillberich2230@gmail.com>
2025-11-18 11:01:32 +0500

    Ko'rish

--- src/pages/Quiz/ListQuiz.jsx
+++ src/pages/Quiz/ListQuiz.jsx
@@ -565,13 +565,14 @@ function ListQuiz() {
                                                 <MdDelete/>
                                             </button>
                                         )}
-
+                                        {role === "admin" && (
                                             <button
                                                 className="bg-blue-500 text-white px-3 py-1 rounded"
                                                 onClick={() => handleOpenResult(quiz?.quiz_id)}
                                             >
                                                 Ko‘rish
                                             </button>
+                                        )}
 
 
                                         {role === "admin" && (
